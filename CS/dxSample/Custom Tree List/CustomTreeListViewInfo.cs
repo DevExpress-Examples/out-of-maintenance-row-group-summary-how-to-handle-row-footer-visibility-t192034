ff7842ce321a29c7c6622a4c3ed74ce57613cea3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/dxSample/Custom Tree List/CustomTreeListViewInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using DevExpress.XtraTreeList;
+using DevExpress.XtraTreeList.ViewInfo;
+
+namespace dxSample {
+    class CustomTreeListViewInfo : TreeListViewInfo {
+        public CustomTreeListViewInfo(TreeList tl ):base(tl) {
+        }
+        protected override Rectangle CalcRowFooterInfo(RowFooterInfo fInfo, System.Drawing.Rectangle bounds, bool isLast = false) {
+            int lvl = fInfo.Node.Level;
+            if (lvl == 2)
+                return bounds;
+            return base.CalcRowFooterInfo(fInfo, bounds, isLast);
+        }
+    }
+}
---
+++ VB/dxSample/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraTreeList.Design
+
+Namespace dxSample
+    Partial Public Class Form1
+        Inherits DevExpress.XtraEditors.XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            InitTreeList()
+        End Sub
+        Private Sub InitTreeList()
+            InitData()
+            InitSummary()
+        End Sub
+        Private Sub InitData()
+            Dim tempVar As New XViews(treeList1)
+        End Sub
+        Private Sub InitSummary()
+            treeList1.Columns(1).RowFooterSummary = DevExpress.XtraTreeList.SummaryItemType.Sum
+            treeList1.Columns(2).RowFooterSummary = DevExpress.XtraTreeList.SummaryItemType.Count
+        End Sub
+    End Class
+End Namespace
